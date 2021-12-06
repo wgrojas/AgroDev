463fe00df8009846635595b1ab832b9276410899
wgrojas <wgrojas1205@gmail.com>
2021-12-06 15:39:32 -0500

    Editar de datos

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,5 +9,5 @@ exports.mongoConnect = () => {
     mongoose.connect(mongoStringConnection);
     mongoose.Promise = global.Promise;
     const dbConnection = mongoose.connection;
-    dbConnection.on("error", console.error.bind(console, "Mongodb connection error"))
+    dbConnection.on("error", console.error.bind(console, "Mongodb connection Error"))
 }
